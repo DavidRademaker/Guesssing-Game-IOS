9975931bacba0917bab50f2235ad7be27996891c
David Rademaker <davidrademaker@Interapts-MacBook-Pro-97.local>
2018-03-09 14:04:31 -0500

    Added feedback label and rules label

--- Guessing Game IOS/ViewController.swift
+++ Guessing Game IOS/ViewController.swift
@@ -10,20 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var userGuessField: UITextField!
+    @IBOutlet weak var rulesLabel: UILabel!
+    @IBOutlet weak var userFeedbackLabel: UILabel!
     
     var randomNumber = Int(arc4random_uniform(100))
+    var userGuess: Int!
+    
+    var rulesText = """
+Guess a number 1-100
+You will have 5 tries to guess the number
+You will be told if the number is lower or higher
+"""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        rulesLabel.text = rulesText
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
     @IBAction func guessButtonPressed(_ sender: Any) {
+        userGuess = Int(userGuessField.text!)
     }
     
 }
